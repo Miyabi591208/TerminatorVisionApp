88a19e6e16267e81e3de9c136771e035ae837b78
=MYB= <47452469+Miyabi591208@users.noreply.github.com>
2026-03-21 20:30:46 +0900

    Add files via upload

---
+++ TerminatorVision/TerminatorVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerminatorVisionApp.swift
+//  TerminatorVision
+//
+//  Created by 齋藤仁志 on 2026/03/21.
+//
+
+import SwiftUI
+
+@main
+struct TerminatorVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TerminatorVisionTests/TerminatorVisionTests.swift
@@ -0,0 +1,17 @@
+//
+//  TerminatorVisionTests.swift
+//  TerminatorVisionTests
+//
+//  Created by 齋藤仁志 on 2026/03/21.
+//
+
+import Testing
+@testable import TerminatorVision
+
+struct TerminatorVisionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
